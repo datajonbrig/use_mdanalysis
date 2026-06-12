47bee95656bab5dd8237d2a7863b3c74273130bc
Audrey Prendergast <audrey.prendergast2004@gmail.com>
2026-06-12 16:03:40 -0400

    Update rmsf.py

--- functions/rmsf.py
+++ functions/rmsf.py
@@ -1,5 +1,6 @@
 #Written by Audrey D. Prendergast
 #UNTESTED
+#NEEDS: modifications to plot residues nicely with multiple chains per rep and averaged
 import matplotlib.pyplot as plt
 import MDAnalysis as mda
 from MDAnalysis.analysis import align, rms
@@ -16,3 +17,13 @@ def calculate_rmsf():
 
     rmsf = rms.RMSF(CA)
     return rmsf
+
+def plot_rmsf():
+    rmsf = rmsf.rmsd.T
+    time = rmsf[1]/1000
+    plt.figure()
+    plt.plot(time, rmsf, color, linestyle='-')
+    plt.title(name)
+    plt.xlabel("Residue Index", fontsize=12)
+    plt.ylabel("RMSF (Å)", fontsize=12)
+    plt.savefig(out)
