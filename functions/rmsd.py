adc37c50cdba24731552ddb1288987345d76d9a6
Audrey Prendergast <audrey.prendergast2004@gmail.com>
2026-06-12 15:26:43 -0400

    Update rmsd.py

---
+++ functions/rmsd.py
@@ -0,0 +1,16 @@
+import MDAnalysis as mda
+import MDAnalysis.analysis.rms as rms
+import matplotlib.pyplot as plt
+
+def run_rmsd():
+    rmsd = rms.RMSD(u, ref, select='backbone', groupselections=['protein'])
+    return rmsd
+
+def plot_rmsd():
+    rmsd = rmsd.rmsd.T
+    time = rmsd[1]/1000
+    plt.figure()
+    plt.plot(time, rmsd, color, linestyle='-')
+    plt.title(name)
+    plt.xlabel("Time (ns)", fontsize=12)
+    plt.ylabel("RMSD (Å)", fontsize=12)
