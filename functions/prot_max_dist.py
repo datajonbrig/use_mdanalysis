e7cacd30718bbb1b4c81e3965323e0848d2f0955
Audrey Prendergast <audrey.prendergast2004@gmail.com>
2026-06-15 17:24:13 -0400

    Create prot_max_dist.py

---
+++ functions/prot_max_dist.py
@@ -0,0 +1,31 @@
+import numpy as np
+import pandas as pd
+import MDAnalysis as mda
+import matplotlib.pyplot as plt
+
+def prot_maxdist():
+    prot = u.select_atoms("protein")
+
+    dist = []
+    time = []
+
+    for ts in u.trajectory:
+        dist_array = mda.analysis.distances.distance_array(prot.positions, prot.positions, box=u.dimensions)
+        dist.append(np.max(dist_array))
+        time.append((ts.time)/1000)
+    
+    maxdist = pd.DataFrame({'Time':time,'Distance (Å)':dist})
+    outname = f'max_prot_dist_{out}.csv'
+    maxdist.to_csv(outname, index=False)
+
+    return dist, time
+
+def plot_maxdist(time, dist):
+    name = f'Maximum Protein-Protein Distance {title}'
+    outname = f'max_prot_dist_{out}.png'
+    plt.plot(time, dist, color=color)
+    plt.xlabel('Time (ns)')
+    plt.ylabel('Distance (Å)')
+    plt.title(name)
+    plt.savefig(outname)
+    plt.close
