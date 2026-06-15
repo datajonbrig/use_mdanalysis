13d3f4d5f20074849482b44572542a9487c4e11c
Audrey Prendergast <audrey.prendergast2004@gmail.com>
2026-06-15 16:44:38 -0400

    Update dssp.py

--- functions/dssp.py
+++ functions/dssp.py
@@ -4,26 +4,54 @@
 
 import mdtraj as mdt
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
 
 def calculate_dssp_resi():
     trajectory = mdt.load(traj, top=top)
-    dssp = mdt.compute_dssp(trajectory, simplified=True)
+    protein = trajectory.atom_slice(trajectory.topology.select("protein"))
+    dssp = mdt.compute_dssp(protein, simplified=True)
 
-    structures = ['H', 'E', 'C']
+    prot_resi = [res for res in trajectory.topology.residues if res.is_protein]
 
-    n_residues = trajectory.n_residues
-    residx = np.arange(n_residues)
+    structures = ['H','E','C']
+    data = {}
 
-    percent_array = np.zeros((len(structures, n_residues)))
-
-    for i, type in enumerate(structures):
-        percent_array[i] = (dssp == type).sum(axis=0) / trajectory.n_frames * 100
-
-def calculate_dssp_global():
-    dsspg = "global dssp"
-    return dsspg
-
-def plot_dssp():
-    plt.plot()
+    for type in structures:
+        data[type] = np.mean(dssp == type, axis=0)
     
+    data['resSeq'] = [res.resSeq for res in prot_resi]
+    data['resName'] = [res.name for res in prot_resi]
+
+    dsspdf = pd.DataFrame(data)
+    avg_dssp = dsspdf.groupby(['resSeq','resName']).mean().reset_index()
+
+    name = f'residue_dssp_{out}.csv'
+    avg_dssp.to_csv(name, index=False)
+    
+    return protein, dssp
+
+def calculate_dssp_global(protein, dssp):
+    nres = protein.n_residues
+    helix = np.sum(dssp == 'H', axis=1) / nres * 100
+    sheet = np.sum(dssp == 'E', axis=1) / nres * 100
+    coil = np.sum(dssp == 'C', axis=1) / nres * 100
+    ns = protein.time / 1000
+
+    gdssp = pd.DataFrame({'Time (ns)':ns,'Helix':helix,'Sheet':sheet,'Coil':coil})
+    name = f'global_dssp_{out}.csv'
+    gdssp.to_csv(name, index=False)
+
+    return helix, sheet, coil, ns
+
+def plot_dssp(ns, helix, sheet, coil):
+    plt.plot(ns, helix, label='Helix', color='r')
+    plt.plot(ns, sheet, label='Beta Sheet', color='b')
+    plt.plot(ns, coil, label='Coil',color='g')
+    name = f'Global DSSP {title}'
+    plt.title(name)
+    plt.legend(loc = "best")
+    plt.xlabel('Time (ns)')
+    plt.ylabel('Secondary Structure Percentage (%)')
+    plt.savefig()
+    plt.close()
